bf27a91d2f1ba55c91d076d997a561984bbc8c3c
Alexandra Lopez <sccp.alopez@gmail.com>
2016-08-25 17:11:24 -0500

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,9 @@
+var numero;  
+         
+numero=prompt("Ingrese el numero : ");
+	
+	if (parseInt(numero)>0){
+		window.alert("este numero es positivo");
+	} 
+	else {window.alert("este numero es negativo")}
+	
